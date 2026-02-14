8413479be9989a5b2885b8a41279b494d5192cf7
rastor65 <rdamianquintero@uniguajira.edu.co>
2026-02-14 02:06:55 -0500

    .

--- jeanpaul-front/src/environments/enviorement.development.ts
+++ jeanpaul-front/src/environments/enviorement.development.ts
@@ -2,3 +2,4 @@ export const environment = {
   production: false,
   API_URI: 'https://jean-paul-backend.up.railway.app',
 };
+
--- jeanpaul-front/src/environments/environment.ts
+++ jeanpaul-front/src/environments/environment.ts
@@ -2,3 +2,4 @@ export const environment = {
   production: true,
   API_URI: 'https://jean-paul-backend.up.railway.app',
 };
+
